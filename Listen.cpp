5e61a05fb4be247435780e783a4b095b651ad72b
Manuel Valentin <manu.valentin.95@gmail.com>
2016-05-09 10:25:30 +0200

    listening functionality moved back to main

--- Listen.cpp
+++ Listen.cpp
@@ -25,73 +25,7 @@ Listen::~Listen()
 
 void Listen::listener() 
 {
-    while(1)
-	{
-        string command;
-        getline(cin, command);
-        if(command == "add" || command == "add event")
-        {
-            cout << "You selected add" << endl;
-            cout << "Select if the event is a \"meeting\" or a \"deadline\"" << endl;
-            getline(cin, command);
-            m.createNewEvent(command);
-        }
-        else
-        {
-            if(command == "update" || command == "update event")
-            {
-                cout << "You selected update" << endl;
-            }
-            else
-            {
-                if(command == "remove" || command == "remove event")
-                {
-                    cout << "You selected remove" << endl;
-                }
-                else
-                {
-                    if(command == "filter" || command == "filter event")
-                    {
-                        cout << "You selected filter" << endl;
-                    }
-                    else
-                    {
-                        if(command == "print" || command == "print event")
-                        {
-                            cout << "You selected print" << endl;
-                        }
-                        else
-                        {
-                            if(command == "check" || command == "check event")
-                            {
-                                cout << "You selected check" << endl;
-                            }
-                            else
-                            {
-                            	if(command == "help")
-                            	{
-                            		cout << "You selected help" << endl;
-                            	}
-                            	else
-                            	{
-                            		if(command == "exit" || command == "kill pingui")
-                               		{
-                                 	   cout << "***********************Goodbye***********************" << endl;
-                                	   return;
-                                	}
-                               		else
-	                                {
-	                                	cout << "***Invalid command!***\n***Digit help for the list of commands***" << endl;
-    	                            }
-                            	}
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        cout << "Waiting for another command from the user" << endl;
-    }
+    
     
     
                 //old code
--- main.cpp
+++ main.cpp
@@ -15,7 +15,73 @@ int main(int argc, char** argv) {
     m.appendToFile("Another day with Pingui "+m.currentTime());    
     
     //initialize listening
-    l.listener();
+    while(1)
+	{
+        string command;
+        getline(cin, command);
+        if(command == "add" || command == "add event")
+        {
+            cout << "You selected add" << endl;
+            cout << "Select if the event is a \"meeting\" or a \"deadline\"" << endl;
+            getline(cin, command);
+            m.createNewEvent(command);
+        }
+        else
+        {
+            if(command == "update" || command == "update event")
+            {
+                cout << "You selected update" << endl;
+            }
+            else
+            {
+                if(command == "remove" || command == "remove event")
+                {
+                    cout << "You selected remove" << endl;
+                }
+                else
+                {
+                    if(command == "filter" || command == "filter event")
+                    {
+                        cout << "You selected filter" << endl;
+                    }
+                    else
+                    {
+                        if(command == "print" || command == "print event")
+                        {
+                            cout << "You selected print" << endl;
+                        }
+                        else
+                        {
+                            if(command == "check" || command == "check event")
+                            {
+                                cout << "You selected check" << endl;
+                            }
+                            else
+                            {
+                            	if(command == "help")
+                            	{
+                            		cout << "You selected help" << endl;
+                            	}
+                            	else
+                            	{
+                            		if(command == "exit" || command == "kill pingui")
+                               		{
+                                 	   cout << "***********************Goodbye***********************" << endl;
+                                	   return 0;
+                                	}
+                               		else
+	                                {
+	                                	cout << "***Invalid command!***\n***Digit help for the list of commands***" << endl;
+    	                            }
+                            	}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        cout << "Waiting for another command from the user" << endl;
+    }
     
 
     return 0;
